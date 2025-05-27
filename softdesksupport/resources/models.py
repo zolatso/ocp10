a8237e045b53644203dc5380a26e6d8d6c53c190
ts1986 <tomsaunders1@gmail.com>
2025-05-27 16:51:21 +0200

    creation of issue and comment models

--- softdesksupport/resources/models.py
+++ softdesksupport/resources/models.py
@@ -1,5 +1,7 @@
+import uuid
 from django.db import models
 from django.conf import settings
+
 
 class Project(models.Model):
     TYPE_CHOICES = [
@@ -20,3 +22,63 @@ class Project(models.Model):
     )
     time_created = models.DateField(auto_now_add=True)
     
+
+class Issue(models.Model):
+    class Priority(models.IntegerChoices):
+        LOW = 1, "LOW"
+        MEDIUM = 2, "MEDIUM"
+        HIGH = 3, "HIGH"
+    class IssueType(models.IntegerChoices):
+        BUG = 1, "BUG"
+        FEATURE = 2, "FEATURE"
+        TASK = 3, "TASK"
+    class Progress(models.IntegerChoices):
+        TO_DO = 1, "To do"
+        IN_PROGRESS = 2, "In progress"
+        FINISHED = 3, "Finished"
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="issues_created"
+    )
+    project = models.ForeignKey(
+        to='Project', 
+        on_delete=models.CASCADE, 
+        related_name="issues"
+    )
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    assigned_to = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="assigned_issues"
+    )
+    priority = models.IntegerField(
+        choices=Priority.choices,
+        default=Priority.MEDIUM)
+    issue_type = models.IntegerField(
+        choices=IssueType.choices,
+        default=IssueType.TASK)
+    issue_type = models.IntegerField(
+        choices=Progress.choices,
+        default=Progress.TO_DO)
+    time_created = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="comments_created"
+    )
+    issue = models.ForeignKey(
+        to='Issue', 
+        on_delete=models.CASCADE, 
+        related_name="comments"
+    )
+    description = models.TextField(max_length=2048, blank=False)
+    time_created = models.DateField(auto_now_add=True)
--- softdesksupport/resources/serializers.py
+++ softdesksupport/resources/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
-from resources.models import Project
+from resources.models import Project, Issue, Comment
 
 class ProjectSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Project
         fields = ['id','title','description','type','time_created']
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id',]
+
+    def validate(self, data):
+        assigned_to_user = data.get('assigned_to')
+        project = data.get('project')
+
+        if assigned_to_user and project:
+            if not project.contributors.filter(pk=assigned_to_user.pk).exists():
+                raise serializers.ValidationError(
+                    {'assigned_to': "The assigned user must be a contributor to this project."}
+                )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = []
+
     
