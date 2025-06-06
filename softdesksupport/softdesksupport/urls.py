0ac3cccbd39e0030f3d2fd3d840a0e43cfa0dcce
ts1986 <tomsaunders1@gmail.com>
2025-06-06 10:48:53 +0200

    bug fix

--- softdesksupport/softdesksupport/urls.py
+++ softdesksupport/softdesksupport/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet, UserRegistrationView, ContributorViewSet
 from resources.views import (
